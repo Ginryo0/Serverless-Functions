526e87395a41600e9ea457c66d2351760a68769d
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-17 16:51:15 +0200

    2nd example done

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -3,10 +3,27 @@ const result = document.querySelector('.result');
 const fetchData = async () => {
   try {
     const { data } = await axios('/api/2-basic-api');
-    const products = data.map(prod => {
-      const {image:}
-    })
-    result.innerHTML = '<h2>yo</h2>';
+    const products = data
+      .map((prod) => {
+        const {
+          image: { url },
+          name,
+          price,
+        } = prod;
+
+        return `<article class="product">
+      <img
+        src="${url}"
+        alt="${name}"
+      />
+      <div class="info">
+        <h5>${name}</h5>
+        <h5 class="price">$${price}</h5>
+      </div>
+    </article>`;
+      })
+      .join('');
+    result.innerHTML = products;
   } catch (error) {
     result.innerHTML = `<h4>Something went wrong! please try again later.</h4>`;
   }
--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,6 +3,6 @@ const items = require('../assets/data');
 exports.handler = async (event, ctx, cb) => {
   return {
     statusCode: 200,
-    body: 'Hello World',
+    body: JSON.stringify(items),
   };
 };
