c78058ac27832c81a80bba240c79592e48649c75
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-17 18:09:55 +0200

    airtable shop finished

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,23 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios('/api/3-airtable');
+    const products = data
+      .map((prod) => {
+        const { id, url, name, price } = prod;
+
+        return `<a href="product.html?id=${id}" class="product">
+      <img src="${url}" alt="${name}"/>
+      <div class="info">
+      <h5>${name}</5>
+      <h5 class="price">$${price}</h5>
+      </div>
+      </a>`;
+      })
+      .join('');
+    result.innerHTML = products;
+  } catch (error) {}
+};
+
+fetchData();
