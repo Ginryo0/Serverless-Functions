1ed7905a51036a8edd0b8c8e148809b8f8cb6fbd
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-18 19:57:10 +0200

    weather function done

--- functions/5-weather.js
+++ functions/5-weather.js
@@ -1,2 +1,28 @@
 require('dotenv').config();
 const axios = require('axios');
+
+const url = `http://api.openweathermap.org/data/2.5/weather?${process.env.OPEN_WEATHER_API_KEY}&units=imperial&q=`;
+
+exports.handler = async (event, context, cb) => {
+  const method = event.httpMethod;
+  if (method !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Invalid Method! Only Post Requests Allowed.',
+    };
+  }
+  const { city } = JSON.parse(event.body);
+  try {
+    const res = await axios.get(`${url}${city}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.data),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(err),
+    };
+  }
+};
