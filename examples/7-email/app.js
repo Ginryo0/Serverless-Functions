b10a8b3460099ba499267763527040812f3a7d1f
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-19 06:15:36 +0200

    mail done

--- examples/7-email/app.js
+++ examples/7-email/app.js
@@ -1,9 +1,52 @@
-const nameInput = document.querySelector('.name-input')
-const emailInput = document.querySelector('.email-input')
-const subjectInput = document.querySelector('.subject-input')
-const messageInput = document.querySelector('.message-input')
-const form = document.querySelector('.form')
-const btn = document.querySelector('.submit-btn')
-const alert = document.querySelector('.alert')
-const title = document.querySelector('.title')
-alert.style.display = 'none'
+const nameInput = document.querySelector('.name-input');
+const emailInput = document.querySelector('.email-input');
+const subjectInput = document.querySelector('.subject-input');
+const messageInput = document.querySelector('.message-input');
+const form = document.querySelector('.form');
+const btn = document.querySelector('.submit-btn');
+const alert = document.querySelector('.alert');
+const title = document.querySelector('.title');
+alert.style.display = 'none';
+
+form.addEventListener('submit', async (e) => {
+  console.log('1');
+  e.preventDefault();
+  alert.style.display = 'none';
+  btn.disabled = true;
+  btn.innerHTML = '<span class="sending"></span>';
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const subject = subjectInput.value;
+  const msg = messageInput.value;
+
+  try {
+    await axios.post('/api/7-email', {
+      name,
+      email,
+      subject,
+      msg,
+    });
+
+    // clear inputs
+    nameInput.value = '';
+    emailInput.value = '';
+    subjectInput.value = '';
+    messageInput.value = '';
+    title.textContent = 'Message Sent';
+    title.insertAdjacentHTML(
+      'afterend',
+      '<h4 class="success">Check your mail for an email from <strong>ginryo16</strong></h4>'
+    );
+
+    setTimeout(() => {
+      title.textContent = 'Send a Message';
+      title.nextElementSibling.remove();
+    }, 2000);
+  } catch (error) {
+    alert.style.display = 'block';
+    alert.textContent = error.response.data;
+  }
+  btn.disabled = false;
+  btn.innerHTML = 'Send';
+});
--- functions/7-email.js
+++ functions/7-email.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+console.log(process.env.SENDGRID_API_KEY);
 
-const { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASSWORD } = process.env;
+// nodemailer doesn't work
+// const nodemailer = require('nodemailer');
+// const { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASSWORD } = process.env;
 
-// create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-  host: EMAIL_HOST,
-  port: EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: EMAIL_USER, // generated ethereal user
-    pass: EMAIL_PASSWORD, // generated ethereal password
-  },
-});
+// // create reusable transporter object using the default SMTP transport
+// let transporter = nodemailer.createTransport({
+//   host: EMAIL_HOST,
+//   port: EMAIL_PORT,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//     user: EMAIL_USER, // generated ethereal user
+//     pass: EMAIL_PASSWORD, // generated ethereal password
+//   },
+// });
 
 exports.handler = async (event, ctx, cb) => {
   const method = event.httpMethod;
@@ -22,34 +26,37 @@ exports.handler = async (event, ctx, cb) => {
       body: 'Only POST Requests Allowed',
     };
   }
-  const { name, email, subject, message } = JSON.parse(event.body);
-  if (!name || !email || !subject || !message) {
+  const { name, email, subject, msg } = JSON.parse(event.body);
+
+  if (!name || !email || !subject || !msg) {
     return {
       statusCode: 422,
       body: 'Please Fill All Fields',
     };
   }
 
-  const emailMsg = {
-    from: '"Captain Potato 🥔" <potata@farm.com>', // sender address
-    to: `${name} <${email}>`, // list of receivers
-    subject: subject, // Subject line
-    html: `<p style={color: blue}>${message}</p>`, // html body
+  const mail = {
+    to: email,
+    from: process.env.EMAIL_HOST,
+    subject: subject,
+    html: `<h2>Hey ${name}!</h2>
+    <p>${msg}</p>`,
   };
+
+  console.log(mail);
   // send mail with defined transport object
   try {
-    let info = await transporter.sendMail({
-      ...emailMsg,
-    });
+    let info = await sgMail.send(mail);
 
     return {
       statusCode: 200,
       body: 'Success',
     };
   } catch (error) {
+    // console.log(error.response.body.errors);
     return {
       statusCode: 400,
-      body: JSON.stringify(error.message),
+      body: JSON.stringify(error.response.body.errors),
     };
   }
 };
